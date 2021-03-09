8c34f3376799bd07a89c242513bc8e28330f9328
Pedro Rodrigues de Lima <pee.rodrigues360@gmail.com>
2021-03-08 22:45:55 -0300

    Added example plot, now removing extra zeros in phi,zeta

--- lima_arx.py
+++ lima_arx.py
@@ -1,6 +1,6 @@
 #%%
 import numpy as np
-from numpy.linalg import inv, pinv
+from numpy.linalg import solve
 from control.matlab import *
 
 def lima_arx(y1,y2,u,nze,npo,nk,Ts):
@@ -14,30 +14,57 @@ def lima_arx(y1,y2,u,nze,npo,nk,Ts):
     y1_d = np.zeros([len(y1), npo])
     y2_d = np.zeros([len(y2), npo])
     for i in range(0, npo):
-        y1_d[i+1:, i] = y1[:-(i+1)]
-        y2_d[i+1:, i] = y2[:-(i+1)]
+        y1_d[i+1:, i] = -y1[:-(i+1)]
+        y2_d[i+1:, i] = -y2[:-(i+1)]
 
-    # Least squares
+    # Regressor vectors
     phi = np.concatenate((u_d, y1_d), axis = 1)
     zeta = np.concatenate((u_d, y2_d), axis = 1)
-    theta = inv(zeta.T@phi)@(zeta.T@y1)
+
+    # Removing extra zeros
+    xtra = 1 + max(npo, nze+nk)
+    phi = phi[xtra:,:]
+    zeta = zeta[xtra:,:]
+    y1 = y1[xtra:]
+
+    A = zeta.T@phi
+    b = zeta.T@y1
+
+    # Least squares
+    theta = np.linalg.solve(A, b) # np.linalg.lstsq?
 
     # Defining identified transfer function
-    num = theta[:nze+1].T
-    den = np.concatenate(([1],theta[nze+1:].T))
+    num = np.concatenate((theta[:nze+1].T, np.zeros([npo-nze])))
+    den = np.concatenate(([1], theta[nze+1:].T, np.zeros(nk)))
     G = tf(num,den,Ts)
 
     return G, theta
 
+# Example
+
+import matplotlib.pyplot as plt
+
 Ts = 1
 t = np.arange(1,50)
 u = np.ones(len(t))
+u[0] = 0
 
 z = tf('z')
 z.dt = Ts
-G = 0.5*z/(z-0.9)/(z-0.8)
+G = 1/(z-0.9)
+
+nze, npo, nk = 0, 1, 1
 
 y = lsim(G,u)[0]
 
-G = lima_arx(y,y,u,1,2,1,Ts)[0]
+Gr, theta = lima_arx(y,y,u,nze,npo,nk,Ts)
+Gr
+
+yr = lsim(Gr,u)[0]
+
+fig, ax = plt.subplots()
+ax.plot(t,u,label='Input data')
+ax.plot(t,y, 'k', label='Output data')
+ax.plot(t,yr, 'r--',label='Model output')
+ax.legend()
 # %%
